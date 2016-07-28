4c700bc6fbc55c7c9aa02d0b2caa1f8bd83817f8
MasonMcGill <mmcgill@caltech.edu>
2016-07-28 01:44:25 -0700

    Implemented figure generation for experiment 1.

--- scripts/lib/desc.py
+++ scripts/lib/desc.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 __all__ = ['net_desc', 'render_net_desc']
 
 ################################################################################
-# Descriptors and Performance Metrics
+# Network Statistics and Performance Metrics
 ################################################################################
 
 def p_cor(net, ℓ):
@@ -20,6 +20,9 @@ def x_ent(net, ℓ, ϵ=1e-6):
     p_cls = ϵ / n_cls + (1 - ϵ) * ℓ.x
     return ℓ.p_ev * -tf.reduce_sum(net.y * tf.log(p_cls), 1)
 
+def mse(net, ℓ):
+    return ℓ.p_ev * tf.reduce_sum(tf.square(ℓ.x - net.y), 1)
+
 def moc(net, ℓ):
     return ℓ.p_ev * ℓ.n_ops
 
@@ -28,6 +31,7 @@ def state_tensors(net):
             **{('p_inc', ℓ): p_inc(net, ℓ) for ℓ in net.leaves},
             'acc': sum(p_cor(net, ℓ) for ℓ in net.leaves),
             'mxe': sum(x_ent(net, ℓ) for ℓ in net.leaves),
+            'mse': sum(mse(net, ℓ) for ℓ in net.leaves),
             'moc': sum(moc(net, ℓ) for ℓ in net.layers)}
 
 def mean_net_state(sess, net, batches):
@@ -40,6 +44,10 @@ def mean_net_state(sess, net, batches):
             sums[k] += np.sum(samples[k], 0)
         count += len(x0)
     return {k: sums[k] / count for k in tensors.keys()}
+
+################################################################################
+# Descriptors
+################################################################################
 
 def layer_desc(ms_tr, ms_ts, ℓ):
     ℓ_is_leaf = len(ℓ.sinks) == 0
@@ -56,22 +64,23 @@ def net_desc(sess, net, dataset):
     ms_ts = mean_net_state(sess, net, dataset.test_batches())
     return {'acc_tr': ms_tr['acc'], 'acc_ts': ms_ts['acc'],
             'mxe_tr': ms_tr['mxe'], 'mxe_ts': ms_ts['mxe'],
+            'mse_tr': ms_tr['mse'], 'mse_ts': ms_ts['mse'],
             'moc_tr': ms_tr['moc'], 'moc_ts': ms_ts['moc'],
             'root': layer_desc(ms_tr, ms_ts, net.root)}
 
 ################################################################################
-# Text Rendering
+# Descriptor Rendering
 ################################################################################
 
-def render_layer(ℓ):
+def render_layer_desc(desc):
     annotation = (
-        '[{:.1%} ✓ {:.1%} ×]'.format(ℓ['p_cor_ts'], ℓ['p_inc_ts'])
-        if len(ℓ['sinks']) == 0 else '')
+        '[{:.1%} ✓ {:.1%} ×]'.format(desc['p_cor_ts'], desc['p_inc_ts'])
+        if len(desc['sinks']) == 0 else '')
     sink_text = ''.join(
-        '\n↳ ' + render_layer(s).replace(
-            '\n', '\n| ' if i < len(ℓ['sinks']) - 1 else '\n  ')
-        for i, s in enumerate(ℓ['sinks']))
-    return '%s %s%s' % (ℓ['type'], annotation, sink_text)
+        '\n↳ ' + render_layer_desc(s).replace(
+            '\n', '\n| ' if i < len(desc['sinks']) - 1 else '\n  ')
+        for i, s in enumerate(desc['sinks']))
+    return '%s %s%s' % (desc['type'], annotation, sink_text)
 
 def render_net_desc(desc, name='Network'):
     return (
@@ -83,8 +92,9 @@ def render_net_desc(desc, name='Network'):
         '⋮\n'
         '⋮   Total accuracy: {:.2%}\n'
         '⋮   Mean cross-entropy: {:.2e}\n'
+        '⋮   Mean squared error: {:.2e}\n'
         '⋮   Mean op count: {:.2e}\n'
         '⋮'
     ).format(
-        name, render_layer(desc['root']).replace('\n', '\n⋮   '),
-        desc['acc_ts'], desc['mxe_ts'], desc['moc_ts'])
+        name, render_layer_desc(desc['root']).replace('\n', '\n⋮   '),
+        desc['acc_ts'], desc['mxe_ts'], desc['mse_ts'], desc['moc_ts'])
