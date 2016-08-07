125ca94c69c13fb3edf2db3ab8f985d6753aa7cc
MasonMcGill <mmcgill@caltech.edu>
2016-08-07 16:19:04 -0700

    Switched to a decaying learning rate.

--- scripts/lib/training.py
+++ scripts/lib/training.py
@@ -8,12 +8,15 @@ from lib.desc import net_desc, render_net_desc
 
 def train(net, dataset, name='Network', batch_size=64,
           n_epochs=100, logging_period=5):
-    train_op = tf.train.AdamOptimizer().minimize(tf.reduce_mean(net.ℓ_tr))
+    net_t = tf.Variable(0.0)
+    λ_learn = 0.1 / 2**(net_t / 10)
+    optimizer = tf.train.MomentumOptimizer(λ_learn, 0.9)
+    train_op = optimizer.minimize(tf.reduce_mean(net.ℓ_tr))
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
         for t in range(n_epochs):
             for x0, y in dataset.training_batches(batch_size):
-                train_op.run({net.x0: x0, net.y: y})
+                train_op.run({net.x0: x0, net.y: y, net_t: t})
             if (t + 1) % logging_period == 0:
                 desc = net_desc(sess, net, dataset)
                 print(render_net_desc(desc, '%s — Epoch %i' % (name, t + 1)))
