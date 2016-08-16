ab5b75fc38aab3375cc62a77a07b464733749302
MasonMcGill <mmcgill@caltech.edu>
2016-08-15 22:33:27 -0700

    Added the option to allocate a validation set.

--- scripts/lib/data.py
+++ scripts/lib/data.py
@@ -18,7 +18,7 @@ def batches(x0, y, n):
 ################################################################################
 
 class Dataset:
-    def __init__(self, path, normalize=True, σ=3, ϵ=1e-3):
+    def __init__(self, path, n_vl=0, normalize=True, ϵ=1e-3):
         archive = io.loadmat(path)
         self.x0_tr = archive['x0_tr']
         self.x0_ts = archive['x0_ts']
@@ -29,6 +29,12 @@ class Dataset:
             self.x0_tr /= np.std(self.x0_tr, (0, 1, 2)) + ϵ
             self.x0_ts -= np.mean(self.x0_tr, (0, 1, 2))
             self.x0_ts /= np.std(self.x0_tr, (0, 1, 2)) + ϵ
+        if n_vl > 0:
+            order = rand.permutation(len(self.x0_tr))
+            self.x0_vl = np.take(self.x0_tr, order[:n_vl], axis=0)
+            self.y_vl = np.take(self.y_tr, order[:n_vl], axis=0)
+            self.x0_tr = np.take(self.x0_tr, order[n_vl:], axis=0)
+            self.y_tr = np.take(self.y_tr, order[n_vl:], axis=0)
 
     @property
     def x0_shape(self):
@@ -43,3 +49,6 @@ class Dataset:
 
     def test_batches(self, n=512):
         yield from batches(self.x0_ts, self.y_ts, n)
+
+    def validation_batches(self, n=512):
+        yield from batches(self.x0_vl, self.y_vl, n)
