f243eefdbb8c4ba298b9535f6038374117155b81
MasonMcGill <mmcgill@caltech.edu>
2017-01-25 16:07:08 -0800

    Added many small improvements developed during the second major round of experiments.

--- scripts/lib/net_types.py
+++ scripts/lib/net_types.py
@@ -8,8 +8,12 @@ import tensorflow as tf
 from lib.layer_types import BatchNorm, Chain, Layer, LinTrans, NoOp, Rect
 
 ################################################################################
-# Optimization
+# Support Functions
 ################################################################################
+
+def n_leaves(ℓ): return (
+    1 if len(ℓ.sinks) == 0
+    else sum(map(n_leaves, ℓ.sinks)))
 
 def params_list_rec(ℓ):
     if ℓ is not None:
@@ -17,12 +21,17 @@ def params_list_rec(ℓ):
         for c in getattr(ℓ, 'comps', []):
             yield from params_list_rec(c)
 
-def minimize_expected(net, cost, optimizer, α_rtr=1):
+def minimize_expectation(layers, cost, optimizer, α_rtr=1, talr=True):
+    lr_scale = lambda ℓ: (
+        1 / tf.sqrt(tf.reduce_mean(tf.square(ℓ.p_tr)))
+        if talr else 1)
     lr_scales = {
-        **{θ: 1 / tf.sqrt(tf.reduce_mean(tf.square(ℓ.p_tr)))
-           for ℓ in net.layers for θ in params_list_rec(ℓ)},
-        **{θ: α_rtr / tf.sqrt(tf.reduce_mean(tf.square(ℓ.p_tr)))
-           for ℓ in net.layers for θ in params_list_rec(ℓ.router)}}
+        **{θ: lr_scale(ℓ)
+           for ℓ in layers
+           for θ in params_list_rec(ℓ)},
+        **{θ: α_rtr * lr_scale(ℓ)
+           for ℓ in layers
+           for θ in params_list_rec(ℓ.router)}}
     grads = optimizer.compute_gradients(cost)
     scaled_grads = [(lr_scales[θ] * g, θ) for g, θ in grads if g is not None]
     return optimizer.apply_gradients(scaled_grads)
@@ -83,43 +92,47 @@ class SRNet(Net):
         self.μ_lrn = tf.placeholder_with_default(ϕ.μ_lrn, ())
         for ℓ in self.layers:
             ℓ.p_ev = tf.ones((tf.shape(self.x0)[0],))
-        c_tr = sum(ℓ.c_err + ℓ.c_mod for ℓ in self.layers)
+        c_tot = tf.reduce_mean(sum(ℓ.c_err + ℓ.c_mod for ℓ in self.layers))
         opt = tf.train.MomentumOptimizer(self.λ_lrn, self.μ_lrn)
-        self.train = opt.minimize(tf.reduce_mean(c_tr))
+        self.train = opt.minimize(c_tot)
 
 ################################################################################
 # Decision Smoothing Networks
 ################################################################################
 
-def route_sinks_ds_stat(ℓ, opts, net):
-    for s in ℓ.sinks:
-        route_ds(s, ℓ.p_tr, ℓ.p_ev, opts, net)
-
-def route_sinks_ds_dyn(ℓ, opts, net):
-    def n_leaves(ℓ): return (
-        1 if len(ℓ.sinks) == 0
-        else sum(map(n_leaves, ℓ.sinks)))
-    def p_tr_ϵ(ℓ):
-        return opts.ϵ * n_leaves(ℓ) / n_leaves(net.root)
-    π_tr = (
-        (1 - p_tr_ϵ(ℓ) / ℓ.p_tr[:, None]) * tf.nn.softmax(ℓ.router.x / opts.τ)
-        + list(map(p_tr_ϵ, ℓ.sinks)) / ℓ.p_tr[:, None])
-    π_ev = tf.to_float(tf.equal(
-        tf.expand_dims(tf.to_int32(tf.argmax(ℓ.router.x, 1)), 1),
-        tf.range(len(ℓ.sinks))))
-    for i, s in enumerate(ℓ.sinks):
-        route_ds(s, ℓ.p_tr * π_tr[:, i], ℓ.p_ev * π_ev[:, i], opts, net)
-
-def route_ds(ℓ, p_tr, p_ev, opts, net):
-    ℓ.p_tr = p_tr
-    ℓ.p_ev = p_ev
-    if len(ℓ.sinks) < 2: route_sinks_ds_stat(ℓ, opts, net)
-    else: route_sinks_ds_dyn(ℓ, opts, net)
-
 class DSNet(Net):
     default_hypers = Ns(
-        k_cpt=0.0, ϵ=1e-6, τ=1.0, λ_em=0.9,
-        λ_lrn=1e-3, μ_lrn=0.9, α_rtr=1.0)
+        k_cpt=0.0, k_dec=0.01, ϵ=1e-6, τ=1.0,
+        λ_lrn=1e-3, μ_lrn=0.9, talr=True, α_rtr=1.0)
+
+    def _route(self, ℓ, p_tr, p_ev):
+        ℓ.p_tr = p_tr
+        ℓ.p_ev = p_ev
+        if len(ℓ.sinks) < 2:
+            self._route_sinks_stat(ℓ)
+        else:
+            self._route_sinks_dyn(ℓ)
+
+    def _route_sinks_stat(self, ℓ):
+        for s in ℓ.sinks:
+            self._route(s, ℓ.p_tr, ℓ.p_ev)
+
+    def _route_sinks_dyn(self, ℓ):
+        def p_tr_ϵ(ℓ):
+            return self.ϵ * n_leaves(ℓ) / n_leaves(self.root)
+        π_tr = (
+            (1 - p_tr_ϵ(ℓ) / ℓ.p_tr[:, None])
+            * tf.nn.softmax(ℓ.router.x / self.τ)
+            + list(map(p_tr_ϵ, ℓ.sinks)) / ℓ.p_tr[:, None])
+        π_ev = tf.to_float(tf.equal(
+            tf.expand_dims(tf.to_int32(tf.argmax(ℓ.router.x, 1)), 1),
+            tf.range(len(ℓ.sinks))))
+        for i, s in enumerate(ℓ.sinks):
+            self._route(s, ℓ.p_tr * π_tr[:, i], ℓ.p_ev * π_ev[:, i])
+
+    def _c_dec(self, ℓ):
+        dims = tuple(range(1, len(ℓ.router.x.get_shape())))
+        return self.hypers.k_dec * tf.reduce_sum(tf.square(ℓ.router.x), dims)
 
     def link(self):
         super().link()
@@ -129,71 +142,77 @@ class DSNet(Net):
         self.ϵ = tf.placeholder_with_default(ϕ.ϵ, ())
         self.τ = tf.placeholder_with_default(ϕ.τ, ())
         n_pts = tf.shape(self.x0)[0]
-        route_ds(self.root, tf.ones((n_pts,)), tf.ones((n_pts,)),
-                 Ns(ϵ=self.ϵ, τ=self.τ), self)
+        self._route(self.root, tf.ones((n_pts,)), tf.ones((n_pts,)))
         c_err = sum(ℓ.p_tr * ℓ.c_err for ℓ in self.layers)
         c_cpt = sum(ℓ.p_tr * ϕ.k_cpt * (ℓ.n_ops + getattr(ℓ.router, 'n_ops', 0))
                     for ℓ in self.layers)
-        c_mod = sum(tf.stop_gradient(ℓ.p_tr) * (ℓ.c_mod + ℓ.router.c_mod)
-                    for ℓ in self.switches)
-        c_tr = c_err + c_cpt + c_mod
+        c_mod = sum(
+            tf.stop_gradient(ℓ.p_tr) * (ℓ.c_mod + getattr(ℓ.router, 'c_mod', 0))
+            for ℓ in self.layers)
+        c_dec = sum(
+            tf.stop_gradient(ℓ.p_tr) * self._c_dec(ℓ)
+            for ℓ in self.switches)
+        c_tot = tf.reduce_mean(c_err + c_cpt + c_mod + c_dec)
         opt = tf.train.MomentumOptimizer(self.λ_lrn, self.μ_lrn)
-        self.train = minimize_expected(
-            self, tf.reduce_mean(c_tr), opt, ϕ.α_rtr)
+        self.train = minimize_expectation(
+            list(self.layers), c_tot, opt,
+            self.hypers.α_rtr, self.hypers.talr)
 
 ################################################################################
 # Actor Networks
 ################################################################################
 
-def route_sinks_ac_stat(ℓ, opts, net):
-    for s in ℓ.sinks:
-        route_ac(s, ℓ.p_tr, ℓ.p_ev, opts, net)
-    ℓ.c_ev = (
-        ℓ.c_err + opts.k_cpt * ℓ.n_ops
-        + sum(s.c_ev for s in ℓ.sinks))
-    ℓ.c_opt = (
-        ℓ.c_err + opts.k_cpt * ℓ.n_ops
-        + sum(s.c_opt for s in ℓ.sinks))
-    ℓ.c_rtr = 0
-
-def route_sinks_ac_dyn(ℓ, opts, net):
-    def n_leaves(ℓ): return (
-        1 if len(ℓ.sinks) == 0
-        else sum(map(n_leaves, ℓ.sinks)))
-    def p_tr_ϵ(ℓ):
-        return opts.ϵ * n_leaves(ℓ) / n_leaves(net.root)
-    π_tr = (
-        (1 - p_tr_ϵ(ℓ) / ℓ.p_tr[:, None]) * tf.nn.softmax(ℓ.router.x / opts.τ)
-        + list(map(p_tr_ϵ, ℓ.sinks)) / ℓ.p_tr[:, None])
-    π_ev = tf.to_float(tf.equal(
-        tf.expand_dims(tf.to_int32(tf.argmax(ℓ.router.x, 1)), 1),
-        tf.range(len(ℓ.sinks))))
-    for i, s in enumerate(ℓ.sinks):
-        route_ac(s, ℓ.p_tr * π_tr[:, i], ℓ.p_ev * π_ev[:, i], opts, net)
-    ℓ.c_ev = (
-        ℓ.c_err + opts.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
-        + sum(π_ev[:, i] * s.c_ev
-              for i, s in enumerate(ℓ.sinks)))
-    ℓ.c_opt = (
-        ℓ.c_err + opts.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
-        + reduce(tf.minimum, (s.c_opt for s in ℓ.sinks)))
-    ℓ.c_rtr = (
-        opts.k_rtr * sum(
-            π_tr[:, i] * tf.stop_gradient(
-                s.c_opt if opts.optimistic else s.c_ev)
-            for i, s in enumerate(ℓ.sinks)))
-
-def route_ac(ℓ, p_tr, p_ev, opts, net):
-    ℓ.p_tr = p_tr
-    ℓ.p_ev = p_ev
-    if len(ℓ.sinks) < 2: route_sinks_ac_stat(ℓ, opts, net)
-    else: route_sinks_ac_dyn(ℓ, opts, net)
-
 class AcNet(Net):
     default_hypers = Ns(
-        k_cpt=0.0, k_rtr=1.0, ϵ=1e-6, τ=1.0,
-        λ_em=0.9, λ_lrn=1e-3, μ_lrn=0.9,
-        α_rtr=1.0, optimistic=False)
+        k_cpt=0.0, k_dec=0.01, ϵ=1e-6, τ=1.0, optimistic=False,
+        λ_lrn=1e-3, μ_lrn=0.9, talr=True, α_rtr=1.0)
+
+    def _route(self, ℓ, p_tr, p_ev):
+        ℓ.p_tr = p_tr
+        ℓ.p_ev = p_ev
+        if len(ℓ.sinks) < 2:
+            self._route_sinks_stat(ℓ)
+        else:
+            self._route_sinks_dyn(ℓ)
+
+    def _route_sinks_stat(self, ℓ):
+        for s in ℓ.sinks:
+            self._route(s, ℓ.p_tr, ℓ.p_ev)
+        ℓ.c_ev = (
+            ℓ.c_err + self.hypers.k_cpt * ℓ.n_ops
+            + sum(s.c_ev for s in ℓ.sinks))
+        ℓ.c_opt = (
+            ℓ.c_err + self.hypers.k_cpt * ℓ.n_ops
+            + sum(s.c_opt for s in ℓ.sinks))
+        ℓ.c_rtr = 0
+
+    def _route_sinks_dyn(self, ℓ):
+        def p_tr_ϵ(ℓ):
+            return self.ϵ * n_leaves(ℓ) / n_leaves(self.root)
+        π_tr = (
+            (1 - p_tr_ϵ(ℓ) / ℓ.p_tr[:, None])
+            * tf.nn.softmax(ℓ.router.x / self.τ)
+            + list(map(p_tr_ϵ, ℓ.sinks)) / ℓ.p_tr[:, None])
+        π_ev = tf.to_float(tf.equal(
+            tf.expand_dims(tf.to_int32(tf.argmax(ℓ.router.x, 1)), 1),
+            tf.range(len(ℓ.sinks))))
+        for i, s in enumerate(ℓ.sinks):
+            self._route(s, ℓ.p_tr * π_tr[:, i], ℓ.p_ev * π_ev[:, i])
+        ℓ.c_ev = (
+            ℓ.c_err + self.hypers.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
+            + sum(π_ev[:, i] * s.c_ev
+                  for i, s in enumerate(ℓ.sinks)))
+        ℓ.c_opt = (
+            ℓ.c_err + self.hypers.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
+            + reduce(tf.minimum, (s.c_opt for s in ℓ.sinks)))
+        ℓ.c_rtr = sum(
+            π_tr[:, i] * tf.stop_gradient(
+                s.c_opt if self.hypers.optimistic else s.c_ev)
+            for i, s in enumerate(ℓ.sinks))
+
+    def _c_dec(self, ℓ):
+        dims = tuple(range(1, len(ℓ.router.x.get_shape())))
+        return self.hypers.k_dec * tf.reduce_sum(tf.square(ℓ.router.x), dims)
 
     def link(self):
         super().link()
@@ -203,71 +222,73 @@ class AcNet(Net):
         self.ϵ = tf.placeholder_with_default(ϕ.ϵ, ())
         self.τ = tf.placeholder_with_default(ϕ.τ, ())
         n_pts = tf.shape(self.x0)[0]
-        route_ac(self.root, tf.ones((n_pts,)), tf.ones((n_pts,)),
-                 Ns(ϵ=self.ϵ, τ=self.τ, k_cpt=ϕ.k_cpt, k_rtr=ϕ.k_rtr,
-                    optimistic=ϕ.optimistic), self)
+        self._route(self.root, tf.ones((n_pts,)), tf.ones((n_pts,)))
         c_err = sum(tf.stop_gradient(ℓ.p_tr) * ℓ.c_err for ℓ in self.layers)
         c_rtr = sum(tf.stop_gradient(ℓ.p_tr) * ℓ.c_rtr for ℓ in self.layers)
-        c_mod = sum(tf.stop_gradient(ℓ.p_tr) * (ℓ.c_mod + ℓ.router.c_mod)
-                    for ℓ in self.switches)
-        c_tr = c_err + c_rtr + c_mod
+        c_mod = sum(
+            tf.stop_gradient(ℓ.p_tr) * (ℓ.c_mod + getattr(ℓ.router, 'c_mod', 0))
+            for ℓ in self.layers)
+        c_dec = sum(
+            tf.stop_gradient(ℓ.p_tr) * self._c_dec(ℓ)
+            for ℓ in self.switches)
+        c_tot = tf.reduce_mean(c_err + c_rtr + c_mod + c_dec)
         opt = tf.train.MomentumOptimizer(self.λ_lrn, self.μ_lrn)
-        self.train = minimize_expected(
-            self, tf.reduce_mean(c_tr), opt, ϕ.α_rtr)
+        self.train = minimize_expectation(
+            list(self.layers), c_tot, opt,
+            self.hypers.α_rtr, self.hypers.talr)
 
 ################################################################################
 # Cost Regression Networks
 ################################################################################
 
-def route_sinks_cr_stat(ℓ, opts, net):
-    for s in ℓ.sinks:
-        route_cr(s, ℓ.p_tr, ℓ.p_ev, opts, net)
-    ℓ.c_ev = (
-        ℓ.c_err + opts.k_cpt * ℓ.n_ops
-        + sum(s.c_ev for s in ℓ.sinks))
-    ℓ.c_opt = (
-        ℓ.c_err + opts.k_cpt * ℓ.n_ops
-        + sum(s.c_opt for s in ℓ.sinks))
-    ℓ.c_cre = 0
-
-def route_sinks_cr_dyn(ℓ, opts, net):
-    def n_leaves(ℓ): return (
-        1 if len(ℓ.sinks) == 0
-        else sum(map(n_leaves, ℓ.sinks)))
-    def p_tr_ϵ(ℓ):
-        return opts.ϵ * n_leaves(ℓ) / n_leaves(net.root)
-    π_tr = (
-        (1 - p_tr_ϵ(ℓ) / ℓ.p_tr[:, None]) * tf.nn.softmax(ℓ.router.x / opts.τ)
-        + list(map(p_tr_ϵ, ℓ.sinks)) / ℓ.p_tr[:, None])
-    π_ev = tf.to_float(tf.equal(
-        tf.expand_dims(tf.to_int32(tf.argmax(ℓ.router.x, 1)), 1),
-        tf.range(len(ℓ.sinks))))
-    for i, s in enumerate(ℓ.sinks):
-        route_cr(s, ℓ.p_tr * π_tr[:, i], ℓ.p_ev * π_ev[:, i], opts, net)
-    ℓ.c_ev = (
-        ℓ.c_err + opts.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
-        + sum(π_ev[:, i] * s.c_ev
-              for i, s in enumerate(ℓ.sinks)))
-    ℓ.c_opt = (
-        ℓ.c_err + opts.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
-        + reduce(tf.minimum, (s.c_opt for s in ℓ.sinks)))
-    ℓ.c_cre = (
-        opts.k_cre * sum(
-            tf.square(ℓ.router.x[:, i] + tf.stop_gradient(
-                s.c_opt if opts.optimistic else s.c_ev))
-            for i, s in enumerate(ℓ.sinks)))
-
-def route_cr(ℓ, p_tr, p_ev, opts, net):
-    ℓ.p_tr = p_tr
-    ℓ.p_ev = p_ev
-    if len(ℓ.sinks) < 2: route_sinks_cr_stat(ℓ, opts, net)
-    else: route_sinks_cr_dyn(ℓ, opts, net)
-
 class CRNet(Net):
     default_hypers = Ns(
-        k_cpt=0.0, k_cre=1e-3, ϵ=1e-6, τ=0.01,
-        λ_em=0.9, λ_lrn=1e-3, μ_lrn=0.9,
-        α_rtr=1.0, optimistic=False)
+        k_cpt=0.0, k_cre=1e-3, ϵ=1e-6, τ=0.01, optimistic=False,
+        λ_lrn=1e-3, μ_lrn=0.9, talr=True, α_rtr=1.0)
+
+    def _route(self, ℓ, p_tr, p_ev):
+        ℓ.p_tr = p_tr
+        ℓ.p_ev = p_ev
+        if len(ℓ.sinks) < 2:
+            self._route_sinks_stat(ℓ)
+        else:
+            self._route_sinks_dyn(ℓ)
+
+    def _route_sinks_stat(self, ℓ):
+        for s in ℓ.sinks:
+            self._route(s, ℓ.p_tr, ℓ.p_ev)
+        ℓ.c_ev = (
+            ℓ.c_err + self.hypers.k_cpt * ℓ.n_ops
+            + sum(s.c_ev for s in ℓ.sinks))
+        ℓ.c_opt = (
+            ℓ.c_err + self.hypers.k_cpt * ℓ.n_ops
+            + sum(s.c_opt for s in ℓ.sinks))
+        ℓ.c_cre = 0
+
+    def _route_sinks_dyn(self, ℓ):
+        def p_tr_ϵ(ℓ):
+            return self.ϵ * n_leaves(ℓ) / n_leaves(self.root)
+        π_tr = (
+            (1 - p_tr_ϵ(ℓ) / ℓ.p_tr[:, None])
+            * tf.nn.softmax(ℓ.router.x / self.τ)
+            + list(map(p_tr_ϵ, ℓ.sinks)) / ℓ.p_tr[:, None])
+        π_ev = tf.to_float(tf.equal(
+            tf.expand_dims(tf.to_int32(tf.argmax(ℓ.router.x, 1)), 1),
+            tf.range(len(ℓ.sinks))))
+        for i, s in enumerate(ℓ.sinks):
+            self._route(s, ℓ.p_tr * π_tr[:, i], ℓ.p_ev * π_ev[:, i])
+        ℓ.c_ev = (
+            ℓ.c_err + self.hypers.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
+            + sum(π_ev[:, i] * s.c_ev
+                  for i, s in enumerate(ℓ.sinks)))
+        ℓ.c_opt = (
+            ℓ.c_err + self.hypers.k_cpt * (ℓ.n_ops + ℓ.router.n_ops)
+            + reduce(tf.minimum, (s.c_opt for s in ℓ.sinks)))
+        ℓ.c_cre = (
+            self.hypers.k_cre * sum(
+                tf.square(ℓ.router.x[:, i] + tf.stop_gradient(
+                    s.c_opt if self.hypers.optimistic else s.c_ev))
+                for i, s in enumerate(ℓ.sinks)))
 
     def link(self):
         super().link()
@@ -277,14 +298,14 @@ class CRNet(Net):
         self.ϵ = tf.placeholder_with_default(ϕ.ϵ, ())
         self.τ = tf.placeholder_with_default(ϕ.τ, ())
         n_pts = tf.shape(self.x0)[0]
-        route_cr(self.root, tf.ones((n_pts,)), tf.ones((n_pts,)),
-                 Ns(ϵ=self.ϵ, τ=self.τ, k_cpt=ϕ.k_cpt, k_cre=ϕ.k_cre,
-                    optimistic=ϕ.optimistic), self)
+        self._route(self.root, tf.ones((n_pts,)), tf.ones((n_pts,)))
         c_err = sum(tf.stop_gradient(ℓ.p_tr) * ℓ.c_err for ℓ in self.layers)
         c_cre = sum(tf.stop_gradient(ℓ.p_tr) * ℓ.c_cre for ℓ in self.layers)
-        c_mod = sum(tf.stop_gradient(ℓ.p_tr) * (ℓ.c_mod + ℓ.router.c_mod)
-                    for ℓ in self.switches)
-        c_tr = c_err + c_cre + c_mod
+        c_mod = sum(
+            tf.stop_gradient(ℓ.p_tr) * (ℓ.c_mod + getattr(ℓ.router, 'c_mod', 0))
+            for ℓ in self.layers)
+        c_tot = tf.reduce_mean(c_err + c_cre + c_mod)
         opt = tf.train.MomentumOptimizer(self.λ_lrn, self.μ_lrn)
-        self.train = minimize_expected(
-            self, tf.reduce_mean(c_tr), opt, ϕ.α_rtr)
+        self.train = minimize_expectation(
+            list(self.layers), c_tot, opt,
+            self.hypers.α_rtr, self.hypers.talr)
